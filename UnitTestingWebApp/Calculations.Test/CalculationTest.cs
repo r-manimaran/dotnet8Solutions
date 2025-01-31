7e90320351e19a81374c1990d4aa38267f6e8729
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-01-30 19:17:22 -0500

    Added Another project for unittesting exploration

---
+++ UnitTestingWebApp/Calculations.Test/CalculationTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CalculationTest
+    {
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+           // Arrange
+            var expected = 3;
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add(1, 2);
+           
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Subtract_SimpleValuesShouldCalculate()
+        {
+            // Arrange
+            var expected = 1;
+            var calc = new Calculator();
+            // Act
+            var result = calc.Subtract(3, 2);
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ UnitTestingWebApp/Calculations/Program.cs
@@ -0,0 +1,10 @@
+﻿
+static void Main(string[] args)
+{
+    Add(1, 2);
+}
+
+static void Add(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
